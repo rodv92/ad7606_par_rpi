b6085c5c8a305f594233a2e897fb53cd91fd48e4
Rodrigo Verissimo <rodv92@gmail.com>
2025-08-11 16:41:02 +0200

    working on ftrace debugging

--- Makefile
+++ Makefile
@@ -1,16 +1,17 @@
 obj-m +=ad7606_par.o
 CFLAGS_ad7606_par.o := -UDEBUG
+EXTRA_CFLAGS += -fno-inline
 
 all: module dt
 	echo Built Device Tree Overlay and kernel module
 
 module:
-	make -C /lib/modules/$(shell uname -r)/build M=$(PWD) modules
+	make -C /lib/modules/$(shell uname -r)/build M=$(PWD) V=1  modules
 dt: ad7606.dts
 	cpp -nostdinc -I /usr/src/linux-headers-6.12.34+rpt-common-rpi/include/ -I arm64  -undef -x assembler-with-cpp  ad7606.dts ad7606.dts.preprocessed
 
 	dtc -@ -Hepapr -I dts -O dtb -o ad7606.dtbo ad7606.dts.preprocessed
 
 clean: 
-	make -C /lib/modules/$(shell uname -r)/build M=$(PWD) clean
-	rm -rf ad7606.dtbo
+	make -C /lib/modules/$(shell uname -r)/build M=$(PWD) V=1 clean
+	rm -rf ad7606.dtbo ad7606.dts.preprocessed
--- load.sh
+++ load.sh
@@ -1,7 +1,38 @@
 #!/bin/bash
-sudo modprobe industrialio
-sudo modprobe industrialio_triggered_buffer
-sudo dtoverlay -v ad7606.dtbo
-sudo insmod  ad7606_par.ko
-sudo vclog -m
+sudo modprobe industrialio # load industrial io kernel module
+sudo modprobe iio-trig-hrtimer # load hrtimer industrial io kernel module
+sudo modprobe industrialio_triggered_buffer # load industrial triggered buffer kernel module
+sudo mkdir /sys/kernel/config/iio/triggers/hrtimer/hrtimer0 # configure one hrtimer instance
+sudo dtoverlay -v ad7606.dtbo #load compiled device tree snippet with ad7606 pin to rpi config
+sudo insmod  ad7606_par.ko #load ad7606 driver module
+sudo sh -c "cat /sys/bus/iio/devices/trigger0/name > /sys/bus/iio/devices/iio\:device0/trigger/current_trigger"
+sudo sh -c "echo 1 > /sys/bus/iio/devices/iio\:device0/scan_elements/in_voltage0_en"
+cat /sys/bus/iio/devices/iio\:device0/in_voltage0_raw
+cat /sys/bus/iio/devices/iio\:device0/in_voltage0_raw
+#sudo vclog -m
+
+#sudo sh -c "dd if=/dev/iio\:device0 bs=2 iflag=fullblock count=100 | hexdump"
+
+#Make sure tracing is disabled during tracing reconfiguration
+echo "disabling tracing and current_tracer"
+sudo sh -c "echo 0 > /sys/kernel/debug/tracing/tracing_on"
+sudo sh -c "echo nop > /sys/kernel/debug/tracing/current_tracer"
+
+sudo sh -c "echo ad7606_* > /sys/kernel/debug/tracing/set_ftrace_filter"
+echo "ad7606_* written to set_ftrace_filter"
+sudo sh -c "echo 2 > /sys/kernel/debug/tracing/max_graph_depth"
+echo "set max_graph_depth to 2"
+
+sudo sh -c "echo function_graph > /sys/kernel/debug/tracing/current_tracer"
+sudo sh -c "echo 1 > /sys/kernel/debug/tracing/tracing_on"
+echo "function_graph enabled in current_tracer, and enabling tracing "
+
+echo "now enabling device buffer"
+sudo sh -c 'echo 1 > /sys/bus/iio/devices/iio\:device0/buffer0/enable'
+echo "buffer enabled"
+
+echo "reading trace_pipe in 2 secs"
+sleep 2
+sudo sh -c "cat /sys/kernel/debug/tracing/trace_pipe > /root/trace.log"
+
 
